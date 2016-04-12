c93454c04e5d6f73c966b264c4b7f90ea4d16a2d
Shinseungsou <wwww432@lycos.co.kr>
2016-04-12 20:08:11 +0900

    initialize app configure

---
+++ app/src/main/java/com/jfsiot/mju/ssangcarpool/MainActivity.java
@@ -0,0 +1,7 @@
+package com.jfsiot.mju.ssangcarpool;
+
+/**
+ * Created by SSS on 2016-04-05.
+ */
+public class MainActivity {
+}
---
+++ app/src/main/java/com/jfsiot/mju/ssangcarpool/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.jfsiot.mju.ssangcarpool;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SSS on 2016-04-05.
+ */
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
