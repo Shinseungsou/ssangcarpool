5d3b5c8add8854e017b5acb5c6c71a664f7c54c8
hmpark <wjrlwjqufdnl@naver.com>
2016-05-17 22:56:07 +0900

    profile list
    profile- departure, destination list change

--- app/src/main/java/com/jfsiot/mju/ssangcarpool/activity2/Room.java
+++ app/src/main/java/com/jfsiot/mju/ssangcarpool/activity2/Room.java
@@ -6,11 +6,13 @@ import java.util.List;
  * Created by User on 2016-04-16.
  */
 public class Room {
-    public String title;
-    public String contents;
+    public String departure_name;
+    public String destination_name;
+    public String date;
 
-    Room(String title, String contents){
-        this.title = title;
-        this.contents = contents;
+    Room(String departure_name, String destination_name, String date){
+        this.departure_name = departure_name;
+        this.destination_name = destination_name;
+        this.date = date;
     }
 }
